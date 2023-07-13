ad00ec19eb49bd6de69f6f91153dabdeb3fbc3d3
teyotobias <teyotobias@utexas.edu>
2023-07-13 13:43:27 -0500

    urls and style updated

--- games/urls.py
+++ games/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'), #maps to view.home view function -> does not exist yet, create it
     path('about/', views.about, name='about'),
     path('games/', views.games_index, name='index'),
-    path('games/<int:finch_id>/', views.games_detail, name='detail')
+    path('games/<int:game_id>/', views.games_detail, name='detail')
 ]
 
