9089b8c1281a3e1c45458633fb3cd4f6dd2b6071
teyotobias <teyotobias@utexas.edu>
2023-07-17 17:06:31 -0500

    User model created as well as purchase functionality

--- games/models.py
+++ games/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -11,6 +14,7 @@ class Game(models.Model):
     price = models.DecimalField(max_digits=4, decimal_places=2)
     cover = models.ImageField(upload_to='images/', default='games/default.jpg')
     recommended = models.BooleanField(default=True)
+    purchased = models.BooleanField(default=False)
 
 
 
@@ -20,3 +24,22 @@ class Game(models.Model):
     
     def get_absolute_url(self):
         return reverse('games_detail', kwargs={'pk': self.pk})
+    
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150)
+    games_in_cart = models.ManyToManyField(Game, blank=True, related_name='users_with_game')
+    total_cost = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    purchased_games = models.ManyToManyField(Game, blank=True, related_name='purchasers')
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()    
--- games/views.py
+++ games/views.py
@@ -3,14 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 # from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views import generic
-# from django.contrib import messages
-from .models import Game
+from django.contrib import messages
+from .models import Game, UserProfile
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-
+from django.shortcuts import get_object_or_404, redirect
 class GameCreate(LoginRequiredMixin, CreateView):
     model = Game
     fields = ['title', 'company', 'description', 'year_released', 'price', 'cover']
@@ -52,8 +53,74 @@ class NotRecommendedGamesList(ListView):
     context_object_name = 'games'
 
     def get_queryset(self):
-        return Game.objects.filter(recommended=False)
+        return Game.objects.filter(recommended=False, purchased=False)
     
+@login_required
+def add_to_cart(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+    user_profile = UserProfile.objects.get(user=request.user)
+    if game not in user_profile.games_in_cart.all():
+        user_profile.games_in_cart.add(game)
+        user_profile.total_cost += game.price
+        user_profile.save()
+        messages.success(request, "Game added to your cart!")
+    else:
+        messages.info(request, "This game is already in your cart")
+    return redirect('display_cart')
+
+@login_required
+def remove_from_cart(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+    user_profile = UserProfile.objects.get(user=request.user)
+    if game in user_profile.games_in_cart.all():
+        user_profile.games_in_cart.remove(game)
+        user_profile.total_cost -= game.price
+        user_profile.save()
+        messages.success(request, "Game removed from your cart!")
+    else:
+        messages.info(request, "This game is not in your cart")
+    return redirect('display_cart')
+
+@login_required
+def display_cart(request):
+    user_profile = UserProfile.objects.get(user=request.user)
+    cart = user_profile.games_in_cart.all()
+    total_cost = user_profile.total_cost
+    context = {
+        'cart': cart,
+        'total_cost': total_cost,
+    }
+    return render(request, 'games/cart.html', context)
+
+@login_required
+def purchase(request):
+    user_profile = UserProfile.objects.get(user=request.user)
+    games_to_purchase = user_profile.games_in_cart.all()
+    user_profile.purchased_games.add(*games_to_purchase)
+    for game in games_to_purchase:
+        game.purchased = True
+        game.save()
+    # user_profile.games_in_cart.clear()
+    # user_profile.total_cost = 0
+    user_profile.save()
+    messages.success(request, "Thank you for your purchase! Your cart is now empty.")
+    return redirect('thank_you')
+
+@login_required
+def thank_you(request):
+    user_profile = UserProfile.objects.get(user=request.user)
+    purchased_games = user_profile.games_in_cart.all()
+    total_cost = user_profile.total_cost
+    context = {
+        'purchased_games': purchased_games,
+        'total_cost': total_cost,
+    }
+    user_profile.games_in_cart.clear()
+    user_profile.total_cost = 0
+    user_profile.save()
+    return render(request, 'games/thank_you.html', context)
+
+
 def home(request):
 
     return render(request, 'home.html')
