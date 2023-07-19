7091b9833f7170ee2848c5232998839d86f86168
teyotobias <teyotobias@utexas.edu>
2023-07-18 20:31:22 -0500

    gitignore added and env file updated in preparation for deployment

--- Game_Cease/settings.py
+++ Game_Cease/settings.py
@@ -28,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY') #hidden for deployment
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS = []
 
 LOGIN_URL = 'login'
 
